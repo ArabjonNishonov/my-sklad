cc5d580e9094b2986df73058efd75324305fed94
Arabjon Nishonov <starc9899@example.com>
2026-09-03 22:10:30 +0500

    change web file

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/salom', function () {
     return "salom";
 });
+
+Route::get('/xayr', function () {
+    return "xayr";
+});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
