51ce52eab4d21ab7262e46cc45ef78ffa3fef82d
Arabjon Nishonov <starc9899@example.com>
2026-09-07 21:12:31 +0500

    add function for mysklad

---
+++ app/Services/RoleService.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Services;
+
+
+class RoleService
+{
+    public function getRoles()
+    {
+        return [
+            'admin' => 'Admin',
+            'user' => 'User',
+            'editor' => 'Editor',
+        ];
+    }
+
+    public function changeRole($user,$data)
+    {
+        $user->syncRoles($data['role']);
+    }
+
+    public function deleteRole($user)
+    {
+        $user->removeRole($user->roles->first());
+    }
+
+
+}
---
+++ database/factories/DeleveryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Delevery;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Delevery>
+ */
+class DeleveryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
